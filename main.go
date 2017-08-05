94865b903bb4b20da992970b61fbe391a6d78dff
dextermoran <dextermoran@gmail.com>
2017-08-04 21:56:46 -0500

    begin to implement os scanning. Wrap my brain around unmarshalling xml to structs

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "./nmapLib"
 
 func main() {
 	fmt.Println("scanning...")
-	hosts, err := nmap.ScanOpenTcpPorts("192.168.2.0/24", "22,80,443")
+	hosts, err := nmap.ScanOpenTcpPorts("192.168.0.0/24", "22,80,443")
 	fmt.Println("scanned!")
 	if err != nil {
 		fmt.Println(err)
--- nmapLib/nmap.go
+++ nmapLib/nmap.go
@@ -11,7 +11,7 @@ import (
 )
 
 func scanOpenTcpPorts(subnet string, ports string) (io.Reader, error) {
-	c := exec.Command("nmap", "-oX", "-", "-sS", subnet, "-p", ports)
+	c := exec.Command("nmap", "-oX", "-", "-sS", "-O", subnet, "-p", ports)
 	cout, cerr := c.Output()
 	if cerr != nil {
 		return nil, fmt.Errorf("need root")
@@ -27,13 +27,24 @@ type xnDoc struct {
 	Hosts []xnHost `xml:"host"`
 }
 
+type xnHost struct {
+	Addrs []xnAddr `xml:"address"`
+	Os []xnOs `xml:"os"`
+}
+
 type xnAddr struct {
 	Addr string `xml:"addr,attr"`
 	Type string `xml:"addrtype,attr"`
+	Os string `xml:"os"`
 }
 
-type xnHost struct {
-	Addrs []xnAddr `xml:"address"`
+type xnOs struct {
+	OsMatch []xnOsMatch `xml:"osmatch"`
+}
+
+type xnOsMatch struct {
+	Name string `xml:"name,attr"`
+	Accuracy string `xml:"accuracy,attr"`
 }
 
 func parseXml(reader io.Reader) (hosts []Host, err error) {
@@ -45,6 +56,12 @@ func parseXml(reader io.Reader) (hosts []Host, err error) {
 		return hosts, derr
 	}
 
+	fmt.Println("!!!!!!!!!!!!!!!!")
+	fmt.Println(doc)
+
+	fmt.Println("$$$$$$$$$$$$$")
+	fmt.Println(doc.Hosts)
+
 	for _, xh := range doc.Hosts {
 		h := Host{}
 		for _, xa := range xh.Addrs {
@@ -55,6 +72,14 @@ func parseXml(reader io.Reader) (hosts []Host, err error) {
 				h.Mac = xa.Addr
 			}
 		}
+		/*
+		for _, xo := range xh.Os {
+			for _, xom := range xo.OsMatch {
+				h.Os = xo.Name
+			}
+		}
+		*/
+
 		if h.Ip != "" && h.Mac != "" {
 			hosts = append(hosts, h)
 		}
@@ -66,6 +91,7 @@ func parseXml(reader io.Reader) (hosts []Host, err error) {
 type Host struct {
 	Mac string
 	Ip string
+	Os string
 }
 
 func ScanOpenTcpPorts(subnet, ports string) (hosts []Host, err error) {
